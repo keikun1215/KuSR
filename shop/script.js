ff15df9f7c58e7f455aa0f967acb2e22909a8717
keikun1215 <116414108+keikun1215@users.noreply.github.com>
2023-03-19 12:22:39 +0900

    Create script.js

---
+++ shop/script.js
@@ -0,0 +1,42 @@
+const stocks = {
+    "rapis": 387
+};
+
+stockstuck(stocks["rapis"], "rapis.webp").forEach(i => {
+    document.getElementById("rapistock").appendChild(i)
+});
+
+[...document.getElementsByClassName("titem")].forEach(pitem => {
+    let item = document.createElement("div")
+    item.classList.add("item")
+    item.classList.add("itmg")
+    let img = document.createElement("img")
+    img.src = "img/" + pitem.getAttribute("name")
+    img.classList.add("ha")
+    item.appendChild(img)
+    let txt = document.createElement("p")
+    txt.textContent = pitem.getAttribute("amount")
+    txt.classList.add("itxt")
+    item.appendChild(txt)
+    pitem.appendChild(item)
+})
+
+function stockstuck(s, n, sc = 64) {
+    let res = []
+    let nmuch = s % sc
+    let much = (s - nmuch) / sc
+    console.log(nmuch, much)
+    for (let _ of ((start, end) => [...Array(end + 1).keys()].slice(start))(1, much)) {
+        let item = document.createElement("div")
+        item.classList.add("titem")
+        item.setAttribute("name", n)
+        item.setAttribute("amount", sc)
+        res.push(item)
+    }
+    let item = document.createElement("div")
+    item.classList.add("titem")
+    item.setAttribute("name", n)
+    item.setAttribute("amount", nmuch)
+    res.push(item)
+    return res
+}
